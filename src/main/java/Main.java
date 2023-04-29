cd60779a184f3513e32273623bf9a5aa19b92bc5
Edvard Englund <edvard.englund@medsci.uu.se>
2023-04-29 10:31:05 +0200

    Working manifest!!!!!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,32 @@
 package main.java;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        doTryWithResources();
     }
+
+    static public Reader openReader(String fileName) throws IOException {
+        return Files.newBufferedReader(Paths.get(fileName));
+    }
+
+    public static void doTryWithResources() {
+        char[] buff = new char[8];
+        int length;
+        try (Reader reader = openReader("/home/edvard/sources/dev/java-external-ref/resources/file1.txt")) {
+            while((length = reader.read(buff)) >= 0) {
+                System.out.println("\nlength: " + length);
+                for(int i=0; i < length; i++)
+                    System.out.print(buff[i]);
+            }
+        } catch(IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
+    }
+
 }
